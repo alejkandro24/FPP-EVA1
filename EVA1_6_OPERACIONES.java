1cf2bcd7a53e7099413e46b5ab7464b4c0a8a62a
ALEJANDROCERNA <L25550280@chihuahua2.tecnm.mx>
2025-09-01 20:54:02 -0600

    Add files via upload

---
+++ EVA1_6_OPERACIONES.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_6_operaciones;
+
+/**
+ *
+ * @author carpi
+ */
+public class EVA1_6_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     // TODO code application logic here
+     double pi, radio, area; // todas son dobles 
+        // ES LO MISMO QUE: 
+        // double pi;
+        // double radio;
+        // double area; 
+        // inicialización 
+        pi = 3.1416;
+        radio = 10;
+        area = pi * (radio * radio);
+        // SALIDA DE DATOS 
+        System.out.println("El area de un circulo");
+        System.out.println(area);
+        
+         
+    }
+    
+}
