c3b46355901c7f3e292591a198caba2b54c33ed1
ALEJANDROCERNA <L25550280@chihuahua2.tecnm.mx>
2025-09-30 16:57:26 -0600

    Add files via upload

---
+++ PARCIAL25550280_EXA1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg1raevaluacíon.parcial.pkg25550280_exa1;
+ import java.util.Scanner;
+/**
+ *
+ * @author carpi
+ */
+public class PARCIAL25550280_EXA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+    public static void main(String[] args) {
+    final static String ACESSO = "1234";
+    final static String SALDO = 5000;
+    
+   
+    Scanner input = new Scanner(System.in);
+   String acesso, saldo;
+   
+   System.out.println(saldo);
+   saldo = input.nextInt();
+   
+   
+   if(acesso.equals(acesso)){
+    System.out.println(saldo);
+    saldo = input.nextLine();
+    if (saldo.equals(saldo)){
+     
+    }
+       
+   }
+    
+
+    
+    
+
+
+        // TODO code application logic here
+    }
+    
+}
