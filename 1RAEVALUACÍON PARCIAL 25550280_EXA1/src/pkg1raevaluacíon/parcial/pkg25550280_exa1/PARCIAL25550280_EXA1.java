c7901f3b67e2cbccedf7d75de854df7fd09c360a
ALEJANDROCERNA <L25550280@chihuahua2.tecnm.mx>
2025-09-30 19:30:26 -0600

    Add files via upload

---
+++ 1RAEVALUACÍON PARCIAL 25550280_EXA1/src/pkg1raevaluacíon/parcial/pkg25550280_exa1/PARCIAL25550280_EXA1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg1raevaluacíon.parcial.pkg25550280_exa1;
+
+/**
+ *
+ * @author carpi
+ */
+public class PARCIAL25550280_EXA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
